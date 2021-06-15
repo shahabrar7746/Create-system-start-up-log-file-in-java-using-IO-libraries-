3e586fa7a44be0864ed6a58f2039aa0395c1f74d
shahabrar7746 <73398955+shahabrar7746@users.noreply.github.com>
2021-06-15 13:20:35 +0530

    previous updates will be added

--- log maker/file.java
+++ log maker/file.java
@@ -4,39 +4,40 @@ import java.util.*;
 public class file {
 
 	public static void main(String[] args) throws IOException {
-		 try {   
+		 try { 
+			 ArrayList<String> al = new ArrayList<>();
 			 Calendar cld = Calendar.getInstance();
 			 File myObj = new File("C:\\Users\\Admin\\system start up.txt");
-		      FileWriter myObj1 = new FileWriter("C:\\Users\\Admin\\system start up.txt");  
-		      if (myObj.createNewFile()) { 
-		    	  String Min = "";
-		         Min = Min.valueOf(cld.get(Calendar.MINUTE));
-		         String hour = "";
-		         hour = hour.valueOf(cld.get(Calendar.HOUR));
-		         String se = "";
-		         se = se.valueOf(cld.get(Calendar.SECOND));
-		         myObj1.write("Your Laptop has Started on ");
-		        myObj1.write(hour + ":" + Min + ":" + se );
-		        
+			
+		        Date date=java.util.Calendar.getInstance().getTime();
+		    String temp =    date.toString();
+		   if(myObj.exists()) {
+			    Scanner s = new Scanner(myObj);
+			    while(s.hasNext()) {
+			    	al.add(s.nextLine());
+			    }
+		    	 
+				 FileWriter myObj1 = new FileWriter("C:\\Users\\Admin\\system start up.txt");
+
+		         for(int i = 0;i<al.size();i++) {
+		        	 myObj1.write(al.get(i) + "\n");
+		         }
+		         myObj1.write("Your Laptop has Started on " + temp + "\n");
+		       
+		      
 		        myObj1.close();
-		      } else {  
-		        
-		        myObj.delete();
-		        myObj.createNewFile();
- myObj.delete();
-		        myObj.createNewFile();
-		        String Min = "";
-		         Min = Min.valueOf(cld.get(Calendar.MINUTE));
-		         String hour = "";
-		         hour = hour.valueOf(cld.get(Calendar.HOUR));
-		         String se = "";
-		         se = se.valueOf(cld.get(Calendar.SECOND));
-		         myObj1.write("Your Laptop has Started on ");
-		        myObj1.write(hour + ":" + Min + ":" + se );
-		        
-		        myObj1.close();
-		      }  
-		    } catch (IOException e) {
+		       
+		       
+		   }else {
+				 FileWriter myObj1 = new FileWriter("C:\\Users\\Admin\\system start up.txt");
+				
+		         myObj1.write("Your Laptop has Started on " + " "+ temp + "\n");
+			       
+		   }
+		    
+		   
+		 }
+		     catch (IOException e) {
 		      System.out.println("An error occurred.");
 		      e.printStackTrace();  
 		    }  
